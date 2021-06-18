3d58be256e879c093fae2d05f2566f74a16986b3
“Yangyiding” <1446375363@qq.com>
2021-06-18 20:09:00 +0800

    第二次提交

---
+++ plane01.py
@@ -0,0 +1,52 @@
+import pygame
+import sys
+import traceback
+import myplane
+import enemy
+import bullet
+import supply
+
+from pygame.locals import *
+from random import *
+
+pygame.init()
+pygame.mixer.init()
+
+bg_size = width, height = 480, 700
+screen = pygame.display.set_mode(bg_size)
+pygame.display.set_caption("2021雷霆出击 V1.0 ---杨乙丁")
+
+background = pygame.image.load("images/background.png").convert()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+#载入游戏背景音乐
+pygame.mixer.music.load("sound/game_music.ogg")
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.play(-1)
+
+def main():
+    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.blit(background, (0, 0))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit:
+        pass
+    except:
+        traceback.print_exc()
+        pygame.quit()
+        input()
